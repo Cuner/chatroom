0fe1319e672fe38cbb9955796269f1cf60d1a236
xiaoqiao1995 <469464794@qq.com>
2015-04-27 10:49:46 +0800

    登录页面
    输入正确账号密码就可以进入聊天页面

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">   
+<html xmlns="http://www.w3.org/1999/xhtml">   
+<head>   
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />   
+<title>欢迎使用AJAX聊天室</title>   
+</head>   
+  
+<body>   
+<form action="chatindex.php" name="form1" method="post">   
+<table width="400" border="1" align="center">   
+  <tr>   
+    <th height="52" colspan="2" scope="col">欢迎使用AJAX聊天室</th>   
+  </tr>   
+  <tr>   
+    <td width="107">用户名：</td>   
+    <td width="277"><input type="text" name="username" /></td>   
+  </tr>   
+  <tr>   
+    <td>密　码：</td>   
+    <td><input type="password" name="password" /></td>   
+  </tr>   
+  <tr>   
+    <td> </td>   
+    <td><input type="submit" value="进入聊天室" /> <input type="reset" value="取消"></td>   
+  </tr>   
+</table>   
+</form>   
+<p align="center">PHP方向 版权所有 copyright 2015</p>   
+</body>   
+</html> 
